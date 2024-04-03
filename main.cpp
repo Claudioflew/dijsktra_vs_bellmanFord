12f4635d62f346b58b6b8c7eaded6652c812f87f
Claudioflew <126118326+Claudioflew@users.noreply.github.com>
2024-04-03 13:16:16 -0700

    Updated

--- main.cpp
+++ main.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <chrono>
 
 using namespace std;
+using namespace std::chrono;
 
 vector<vector<pair<int, int>>> makeAdjList(int, vector<vector<int>>&);
 int findCheapestFlightWithDijkstra(int, vector<vector<pair<int, int>>>&, int, int);
@@ -11,14 +13,27 @@ int findCheapestFlightWithBellmanFord(int, vector<vector<pair<int, int>>>&, int,
 
 int main() {
     vector<vector<int>> flights = {{0, 1, 100}, {1, 2, 100}, {2, 0, 100}, {1, 3, 600}, {2, 3, 200}};
+    vector<vector<int>> flights2 = {{0, 1, 500}, {0, 2, 700}, {2, 1, -400}, {1, 3, 100}};
+
 
     vector<vector<pair<int, int>>> adjList = makeAdjList(4, flights);
+    vector<vector<pair<int, int>>> adjList2 = makeAdjList(4, flights2);
 
-    int answerByDijkstra = findCheapestFlightWithDijkstra(4, adjList, 0, 3);
-    int answerByBellmanFord = findCheapestFlightWithBellmanFord(4, adjList, 0, 3);
+    auto start = high_resolution_clock::now();
+    int answerByDijkstra = findCheapestFlightWithDijkstra(4, adjList2, 0, 3);
+    auto stop = high_resolution_clock::now();
+    auto durationOfDijkstra = duration_cast<microseconds>(stop - start);
+
+    start = high_resolution_clock::now();
+    int answerByBellmanFord = findCheapestFlightWithBellmanFord(4, adjList2, 0, 3);
+    stop = high_resolution_clock::now();
+    auto durationOfBellmanFord = duration_cast<microseconds>(stop - start);
 
     cout << "Shortest cost by Dijkstra: " << answerByDijkstra << endl;
     cout << "Shortest cost by Bellman Ford: " << answerByBellmanFord << endl;
+    cout << endl;
+    cout << "Runtime of Dijkstra: " << durationOfDijkstra.count() << endl;
+    cout << "Runtime of Bellman-Ford: " << durationOfBellmanFord.count() << endl;
 }
 
 vector<vector<pair<int, int>>> makeAdjList(int n, vector<vector<int>>& flights) {
@@ -30,6 +45,7 @@ vector<vector<pair<int, int>>> makeAdjList(int n, vector<vector<int>>& flights) 
 }
 
 int findCheapestFlightWithDijkstra(int n, vector<vector<pair<int, int>>>& adjList, int src, int dst) {
+    vector<bool> visited(n, false);
     unordered_map<int, int> costs;
     priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> minHeap;
 
@@ -38,20 +54,21 @@ int findCheapestFlightWithDijkstra(int n, vector<vector<pair<int, int>>>& adjLis
         else costs.insert({i, INT_MAX});
     }
 
-    minHeap.push({src, costs[src]});
+    minHeap.push({costs[src], src});
 
     while (!minHeap.empty()) {
         auto current = minHeap.top();
-        int airport = current.first;
-        int cost = current.second;
+        int cost = current.first;
+        int airport = current.second;
+        visited[airport] = true;
         minHeap.pop();
 
         for (auto neighbor : adjList[airport]) {
             int neighborApt = neighbor.first;
             int flightCost = neighbor.second;
-            if (cost + flightCost < costs[neighborApt]) {
+            if (!visited[neighborApt] && cost + flightCost < costs[neighborApt]) {
                 costs[neighborApt] = cost + flightCost;
-                minHeap.push({neighborApt, costs[neighborApt]});
+                minHeap.push({costs[neighborApt], neighborApt});
             }
         }
     }
