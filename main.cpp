ce66fe9d3a6b12dff70ee8e18b06374f8fb39363
Claudioflew <126118326+Claudioflew@users.noreply.github.com>
2024-04-05 18:23:24 -0700

    Documented

--- main.cpp
+++ main.cpp
@@ -40,8 +40,7 @@ int main() {
  * Converts vector<vector<int>> to an adjacency list vector<vector<pair<int, int>>>
  * Receives: n: Number of vertices (= airports) in the graph
  *           flights: Represents edges, {{Origin Apt, Dest Apt, Cost}, {Origin Apt, Dest Apt, Cost}, ... }
- * Returns: adjList: {Origin Apt: {Dest Apt, Cost}, {Dest Apt, Cost}, Origin Apt: {Dest Apt, Cost}... }
- */
+ * Returns: adjList: {Origin Apt: {Dest Apt, Cost}, {Dest Apt, Cost}, Origin Apt: {Dest Apt, Cost}... } */
 vector<vector<pair<int, int>>> makeAdjList(int n, const vector<vector<int>>& flights) {
     vector<vector<pair<int, int>>> adjList(n);
     // (n) creates n empty vectors in adjList, without this, push_back() won't work.
@@ -57,8 +56,7 @@ vector<vector<pair<int, int>>> makeAdjList(int n, const vector<vector<int>>& fli
  *            adjList: Adjacency list, please refer to makeAdjList()
  *            src: Origin airport
  *            dst: Destination airport
- * Returns: costs[dst]: Cheapest cost from src to dst
- */
+ * Returns: costs[dst]: Cheapest cost from src to dst */
 int findCheapestFlightWithDijkstra(int n, const vector<vector<pair<int, int>>>& adjList, int src, int dst) {
     vector<bool> visited(n, false); // We don't really need this boolean vector for a graph only with non-negative weights
     unordered_map<int, int> costs; // Stores the cheapest cost from src
@@ -99,8 +97,7 @@ int findCheapestFlightWithDijkstra(int n, const vector<vector<pair<int, int>>>& 
  *            adjList: Adjacency list, please refer to makeAdjList()
  *            src: Origin airport
  *            dst: Destination airport
- * Returns: costs[dst]: Cheapest cost from src to dst
- */
+ * Returns: costs[dst]: Cheapest cost from src to dst */
 int findCheapestFlightWithBellmanFord(int n, const vector<vector<pair<int, int>>>& adjList, int src, int dst) {
     unordered_map<int, int> costs;
 
